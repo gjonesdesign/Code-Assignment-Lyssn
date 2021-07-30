2d6b50450356cd849d94312a0346214312a1da0c
gjonesdesign <mail@gjones.design>
2021-07-30 14:06:36 -0400

    completed second step, added id popups when a user is clicked

--- index.js
+++ index.js
@@ -14,7 +14,26 @@ var user_list = document.getElementById("user-list");
 function display(user_array) {
 	for (var i = 0; i < user_array.length; i++) {
 		var li = document.createElement("li");
-		li.innerHTML = user_array[i].lname + ', ' + user_array[i].fname;
+		let index = user_array[i];
+		if (user_array[i].isTherapist) {
+			li.innerHTML = index.lname + ', ' + index.fname + "<span class='material-icons orange'>medication</span>";
+		} else {
+			li.innerHTML = index.lname + ', ' + index.fname + "<span class='material-icons purple'>info</span>";
+		}
+		li.addEventListener("click", function () {
+			display_id(index.id)
+		});
 		user_list.appendChild(li);
 	}
+}
+
+function display_id(id) {
+	if (document.getElementById("id")) {
+		user_list.removeChild(document.getElementById("id"));
+	}
+	var dialog_box = document.createElement("dialog");
+	dialog_box.id = "id";
+	dialog_box.setAttribute("open", "");
+	dialog_box.innerHTML = "<p>User ID: " + id + "</p>";
+	user_list.appendChild(dialog_box);
 }
